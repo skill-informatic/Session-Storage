03302b7b8edf50d3a7f4d7ad17e3970d5f60c911
Mario1613 <mariojhaziel25@gmail.com>
2024-11-08 20:26:38 -0600

    comentarios

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-// Obtener los botones y el estado del tema
-const temaOscuroBtn = document.querySelector("#tema-oscuro");
-const temaClaroBtn = document.querySelector("#tema-claro");
-const estadoElemento = document.querySelector("#estado");
+// Obtener los botones para cambiar entre temas y el elemento para mostrar el estado del tema
+const temaOscuroBtn = document.querySelector("#tema-oscuro"); // Botón para el tema oscuro
+const temaClaroBtn = document.querySelector("#tema-claro"); // Botón para el tema claro
+const estadoElemento = document.querySelector("#estado"); // Elemento para mostrar el estado del tema (claro/oscuro)
 
+// El código dentro de `DOMContentLoaded` se ejecuta cuando el contenido del DOM está completamente cargado
 document.addEventListener("DOMContentLoaded", () => {
-  // Cambiar el tema y guardarlo en sessionStorage
   function cambiarTema(tema) {
     console.log(tema);
-    document.body.className = tema + "-mode";
+    // Cambiar la clase del body para aplicar el tema correspondiente
+    document.body.className = tema;
+    // Guardar el tema en sessionStorage para que persista durante la sesión actual del navegador
     sessionStorage.setItem("tema", tema);
-    estadoElemento.textContent = tema; // Usamos el objeto para mostrar el nombre capitalizado
+
+    estadoElemento.textContent = tema;
   }
 
-  // Al cargar la página, aplicar el tema guardado si existe
+  // Al cargar la página, obtener el tema guardado en sessionStorage (si existe) o usar "light" como valor predeterminado
   const temaGuardado = sessionStorage.getItem("tema") || "light";
+
   cambiarTema(temaGuardado);
 
-  // Asignar los eventos para cambiar el tema
+  // Al hacer clic en el botón del tema oscuro, se cambia a "dark"
   temaOscuroBtn.addEventListener("click", (event) =>
     cambiarTema(event.target.value)
   );
+
   temaClaroBtn.addEventListener("click", (event) =>
     cambiarTema(event.target.value)
   );
